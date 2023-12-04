6ef5e92f2b7c695799a42229e1bf8ac0a34b8334
Syuugo <pub@s1204.me>
2023-12-05 01:49:51 +0900

    ナビゲーションバーを表示させる
    Signed-off-by: Syuugo <pub@s1204.me>

--- app/src/main/java/jp/co/benesse/touch/setuplogin/RecentsActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/RecentsActivity.java
@@ -3,11 +3,13 @@ package jp.co.benesse.touch.setuplogin;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import static android.provider.Settings.System.putInt;
 
 public class RecentsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         finishAndRemoveTask();
+        putInt(this, "hide_navigation_bar", 0);
         startActivity(new Intent().setClassName("com.android.launcher3", "com.android.quickstep.RecentsActivity"));
     }
 }

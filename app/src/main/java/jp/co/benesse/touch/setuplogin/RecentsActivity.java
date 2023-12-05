772e6459dcdc8258eeabdc5d18c053d89ddb9bc2
Syuugo <pub@s1204.me>
2023-12-05 23:17:57 +0900

    ナビバー表示機能を削除
    Signed-off-by: Syuugo <pub@s1204.me>

--- app/src/main/java/jp/co/benesse/touch/setuplogin/RecentsActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/RecentsActivity.java
@@ -3,13 +3,11 @@ package jp.co.benesse.touch.setuplogin;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import static android.provider.Settings.System.putInt;
 
 public class RecentsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         finish();
-        putInt(getContentResolver(), "hide_navigation_bar", 0);
         startActivity(new Intent().setClassName("com.android.launcher3", "com.android.quickstep.RecentsActivity"));
     }
 }

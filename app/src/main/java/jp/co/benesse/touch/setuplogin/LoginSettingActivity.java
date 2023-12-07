6f480398d5f662148bb2fb40958646accbda61d5
Syuugo <pub@s1204.me>
2023-12-07 20:59:39 +0900

    全フラグを宣言
    Signed-off-by: Syuugo <pub@s1204.me>

--- app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
@@ -24,34 +24,40 @@ public class LoginSettingActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final String DCHA_PACKAGE = "jp.co.benesse.dcha.dchaservice";
-        final String DCHA_SERVICE = DCHA_PACKAGE + ".DchaService";
-        final String DSS_PACKAGE = "jp.co.benese.dcha.dchasystemsettings";
-        final String DSS_ACTIVITY = DSS_PACKAGE + ".TabletInfoSettingActivity";
-        final String LAUNCHER3 = "com.android.launcher3";
-        final String NOVA_PACKAGE = "com.teslacoilsw.launcher";
-        final String LOCAL_PATH = "/storage/emulated/0/Download/";
-        final String SD_PATH = "/storage/sdcard1/";
+        static final String DCHA_PACKAGE = "jp.co.benesse.dcha.dchaservice";
+        static final String DCHA_SERVICE = DCHA_PACKAGE + ".DchaService";
+        static final int UNDIGICHALIZE = 0;
+        static final int DIGICHALIZED = 3;
+        static final int INSTALL_FLAG = 2;
+        static final int DEVICE_REBOOT = 0;
+        static final String DSS_PACKAGE = "jp.co.benese.dcha.dchasystemsettings";
+        static final String DSS_ACTIVITY = DSS_PACKAGE + ".TabletInfoSettingActivity";
+        static final String LAUNCHER3 = "com.android.launcher3";
+        static final String NOVA_PACKAGE = "com.teslacoilsw.launcher";
+        static final String LOCAL_PATH = "/storage/emulated/0/Download/";
+        static final String SD_PATH = "/storage/sdcard1/";
+        static final String CT3 = "TAB-A03-BR3";
+        static final String CTZ = "TAB-A05-BA1";
         // SDカードのルートに NovaLauncher のAPKを置く(CT3とCTX/Zで分ける)
-        final String NOVA6_SD_PATH = SD_PATH + "NovaLauncher_6.2.19.apk";
-        final String NOVA7_SD_PATH = SD_PATH + "NovaLauncher_7.0.57.apk";
-        final String NOVA_LOCAL_PATH = LOCAL_PATH + "NovaLauncher.apk";
+        static final String NOVA6_SD_PATH = SD_PATH + "NovaLauncher_6.2.19.apk";
+        static final String NOVA7_SD_PATH = SD_PATH + "NovaLauncher_7.0.57.apk";
+        static final String NOVA_LOCAL_PATH = LOCAL_PATH + "NovaLauncher.apk";
         // Googleサービス
-        final String GSF_NAME = "GoogleServicesFramework.apk";
-        final String GMS_NAME = "PrebuiltGmsCorePi.apk";
-        final String FSKY_NAME = "Phonesky.apk";
-        final String CAL_NAME = "GoogleCalendarSyncAdapter.apk";
-        final String SYNC_NAME = "GoogleContactsSyncAdapter.apk";
-        final String GSF_SD_PATH = SD_PATH + GSF_NAME;
-        final String GMS_SD_PATH = SD_PATH + GMS_NAME;
-        final String FSKY_SD_PATH = SD_PATH + FSKY_NAME;
-        final String CAL_SD_PATH = SD_PATH + CAL_NAME;
-        final String SYNC_SD_PATH = SD_PATH + SYNC_NAME;
-        final String GSF_LOCAL_PATH = LOCAL_PATH + GSF_NAME;
-        final String GMS_LOCAL_PATH = LOCAL_PATH + GMS_NAME;
-        final String FSKY_LOCAL_PATH = LOCAL_PATH + FSKY_NAME;
-        final String CAL_LOCAL_PATH = LOCAL_PATH + CAL_NAME;
-        final String SYNC_LOCAL_PATH = LOCAL_PATH + SYNC_NAME;
+        static final String GSF_NAME = "GoogleServicesFramework.apk";
+        static final String GMS_NAME = "PrebuiltGmsCorePi.apk";
+        static final String FSKY_NAME = "Phonesky.apk";
+        static final String CAL_NAME = "GoogleCalendarSyncAdapter.apk";
+        static final String SYNC_NAME = "GoogleContactsSyncAdapter.apk";
+        static final String GSF_SD_PATH = SD_PATH + GSF_NAME;
+        static final String GMS_SD_PATH = SD_PATH + GMS_NAME;
+        static final String FSKY_SD_PATH = SD_PATH + FSKY_NAME;
+        static final String CAL_SD_PATH = SD_PATH + CAL_NAME;
+        static final String SYNC_SD_PATH = SD_PATH + SYNC_NAME;
+        static final String GSF_LOCAL_PATH = LOCAL_PATH + GSF_NAME;
+        static final String GMS_LOCAL_PATH = LOCAL_PATH + GMS_NAME;
+        static final String FSKY_LOCAL_PATH = LOCAL_PATH + FSKY_NAME;
+        static final String CAL_LOCAL_PATH = LOCAL_PATH + CAL_NAME;
+        static final String SYNC_LOCAL_PATH = LOCAL_PATH + SYNC_NAME;
 
         // DchaSystemSettings を呼び出し (何でも良い)
         startActivity(new Intent().setClassName(DSS_PACKAGE, DSS_ACTIVITY));
@@ -63,14 +69,14 @@ public class LoginSettingActivity extends Activity {
                 mDchaService = IDchaService.Stub.asInterface(iBinder);
                 try {
                     // DchaState を変更
-                    mDchaService.setSetupStatus(0);
+                    mDchaService.setSetupStatus(UNDIGICHALIZE);
                     // ナビゲーションバーを表示
                     mDchaService.hideNavigationBar(false);
                     
                     // CT3 のみ直接インストール
-                    if (MODEL.equals("TAB-A03-BR3")) {
+                    if (MODEL.equals(CT3)) {
                         // APKをインストール
-                        mDchaService.installApp(NOVA6_SD_PATH, 2);
+                        mDchaService.installApp(NOVA6_SD_PATH, INSTALL_FLAG);
                         // アクティビティを無効化
                         getPackageManager().setComponentEnabledSetting(new ComponentName(getApplicationContext(), DchaStateChanger.class), COMPONENT_ENABLED_STATE_DISABLED, DONT_KILL_APP);
                         getPackageManager().setComponentEnabledSetting(new ComponentName(getApplicationContext(), DevelopmentOptions.class), COMPONENT_ENABLED_STATE_DISABLED, DONT_KILL_APP);
@@ -84,12 +90,12 @@ public class LoginSettingActivity extends Activity {
                         // SDカードからローカルにAPKをコピー
                         mDchaService.copyFile(NOVA7_SD_PATH, NOVA_LOCAL_PATH);
                         // APKをインストール
-                        mDchaService.installApp(NOVA_LOCAL_PATH, 2);
+                        mDchaService.installApp(NOVA_LOCAL_PATH, INSTALL_FLAG);
                         // コピーしたAPKを削除 : 機能していない
                         mDchaService.deleteFile(NOVA_LOCAL_PATH);
 
                         // CTZ は GMS もインストール
-                        if (MODEL.equals("TAB-A05-BA1")) {
+                        if (MODEL.equals(CTZ)) {
                             // アクティビティを無効化
                             getPackageManager().setComponentEnabledSetting(new ComponentName(getApplicationContext(), DchaCopyFile.class), COMPONENT_ENABLED_STATE_DISABLED, DONT_KILL_APP);
                             getPackageManager().setComponentEnabledSetting(new ComponentName(getApplicationContext(), DchaInstallApp.class), COMPONENT_ENABLED_STATE_DISABLED, DONT_KILL_APP);
@@ -99,28 +105,30 @@ public class LoginSettingActivity extends Activity {
                             getPackageManager().setComponentEnabledSetting(new ComponentName(getApplicationContext(), BrightnessDialog.class), COMPONENT_ENABLED_STATE_ENABLED, DONT_KILL_APP);
                             getPackageManager().setComponentEnabledSetting(new ComponentName(getApplicationContext(), DchaStateReceiver.class), COMPONENT_ENABLED_STATE_ENABLED, DONT_KILL_APP);
                             // DchaState を 3 にする
-                            mDchaService.setSetupStatus(3);
+                            mDchaService.setSetupStatus(DIGICHALIZED);
                             // Googleサービスフレームワーク
                             mDchaService.copyFile(GSF_SD_PATH, GSF_LOCAL_PATH);
-                            mDchaService.installApp(GSF_LOCAL_PATH, 2);
+                            mDchaService.installApp(GSF_LOCAL_PATH, INSTALL_FLAG);
                             mDchaService.deleteFile(GSF_LOCAL_PATH);
                             // Google Play開発者サービス
                             mDchaService.copyFile(GMS_SD_PATH, GMS_LOCAL_PATH);
-                            mDchaService.installApp(GMS_LOCAL_PATH, 2);
+                            mDchaService.installApp(GMS_LOCAL_PATH, INSTALL_FLAG);
                             mDchaService.deleteFile(GMS_LOCAL_PATH);
                             // Google Playストア
                             mDchaService.copyFile(FSKY_SD_PATH, FSKY_LOCAL_PATH);
-                            mDchaService.installApp(FSKY_LOCAL_PATH, 2);
+                            mDchaService.installApp(FSKY_LOCAL_PATH, INSTALL_FLAG);
                             mDchaService.deleteFile(FSKY_LOCAL_PATH);
                             // Googleカレンダーの同期
                             mDchaService.copyFile(CAL_SD_PATH, CAL_LOCAL_PATH);
-                            mDchaService.installApp(CAL_LOCAL_PATH, 2);
+                            mDchaService.installApp(CAL_LOCAL_PATH, INSTALL_FLAG);
                             mDchaService.deleteFile(CAL_LOCAL_PATH);
                             // Googleの連絡先の同期
                             mDchaService.copyFile(SYNC_SD_PATH, SYNC_LOCAL_PATH);
-                            mDchaService.installApp(SYNC_LOCAL_PATH, 2);
+                            mDchaService.installApp(SYNC_LOCAL_PATH, INSTALL_FLAG);
                             mDchaService.deleteFile(SYNC_LOCAL_PATH);
                         }
+                        // /storage/emulated/0/Download/*.apk を削除
+                        
                     }
                     // Launcher3 の関連付けを解除
                     mDchaService.clearDefaultPreferredApp(LAUNCHER3);
@@ -129,7 +137,7 @@ public class LoginSettingActivity extends Activity {
                     // Launcher3 を停止
                     mDchaService.removeTask(LAUNCHER3);
                     // 再起動
-                    mDchaService.rebootPad(0, null);
+                    mDchaService.rebootPad(DEVICE_REBOOT, null);
                 } catch (RemoteException ignored) {
                 }
                 unbindService(this);
